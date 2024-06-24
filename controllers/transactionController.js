1567cde7f4aa8723eca748443b94ab9bd8414a51
Lesly-Nupert <lesly.nupert@gmail.com>
2024-06-24 22:33:09 +0200

    fonctionnalité addIncome DONE

---
+++ controllers/transactionController.js
@@ -0,0 +1,24 @@
+const { User, Account, Transaction } = require('../models');
+// Sanitize les données pour contrer attaque XSS
+const sanitizeHtml = require('sanitize-html');
+
+const transactionController = {
+    // Créer un revenu
+    addIncome: async (req, res) => {
+        try {
+            const newIncome = await Transaction.create({
+                amount: sanitizeHtml(req.body.amount),
+                name: sanitizeHtml(req.body.name),
+                type: 'revenus',
+                account_id: req.params.id,
+            });
+            res.status(201).json(newIncome);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "Erreur serveur" });
+        }
+    },
+
+};
+
+module.exports = transactionController;
