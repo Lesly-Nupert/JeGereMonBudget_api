8447eb1e0dd588004483852ff7e948cf19046e21
Lesly-Nupert <lesly.nupert@gmail.com>
2024-07-14 04:44:03 +0200

    Déploiement OK

--- database/clientPg.js
+++
@@ -1,5 +0,0 @@
-// CONNEXION PostgreSQL via PG
-// const pg = require('pg');
-// const client = new pg.Client(process.env.DATABASE_URL);
-// client.connect();
-// module.exports = client;
--- database/create.sql
+++ database/create.sql
@@ -1,51 +1,8 @@
 -- PRODUCTION
 
--- BEGIN;
-
--- CREATE TABLE IF NOT EXISTS "user" (
---   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---   "username" varchar(30) NOT NULL,
---   "email" varchar(100) NOT NULL UNIQUE,
---   "password" varchar(255) NOT NULL,
---   "created_at" timestamptz NOT NULL DEFAULT now(),
---   "updated_at" timestamptz,
---   CONSTRAINT username_length_check CHECK (char_length(username) BETWEEN 3 AND 30)
--- );
-
--- CREATE TABLE IF NOT EXISTS "account" (
---   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---   "account_name" varchar(30) NOT NULL,
---   "balance" numeric(10, 2) NOT NULL DEFAULT 0.00,
---   "user_id" smallint NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE,
---   "created_at" timestamptz NOT NULL DEFAULT now(),
---   "updated_at" timestamptz,
---   CONSTRAINT account_name_length_check CHECK (char_length(account_name) BETWEEN 3 AND 30)
--- );
-
--- CREATE TABLE IF NOT EXISTS "transaction" (
---   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---   "transaction_name" varchar(30) NOT NULL,
---   "amount" numeric(10, 2) NOT NULL,
---   "type" varchar(10) NOT NULL CHECK (type IN ('revenus', 'depenses')),
---   "user_id" smallint NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE,
---   "account_id" smallint NOT NULL REFERENCES "account" ("id") ON DELETE CASCADE,
---   "created_at" timestamptz NOT NULL DEFAULT now(),
---   "updated_at" timestamptz,
---   CONSTRAINT transaction_name_length_check CHECK (char_length(transaction_name) BETWEEN 3 AND 30)
-  
--- );
--- COMMIT;
-
-
-
-
-
--- DEVELOPPEMENT
 BEGIN;
 
-DROP TABLE IF EXISTS "user", "account", "transaction" CASCADE;
-
-CREATE TABLE "user" (
+CREATE TABLE IF NOT EXISTS "user" (
   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   "username" varchar(30) NOT NULL,
   "email" varchar(100) NOT NULL UNIQUE,
@@ -55,7 +12,7 @@ CREATE TABLE "user" (
   CONSTRAINT username_length_check CHECK (char_length(username) BETWEEN 3 AND 30)
 );
 
-CREATE TABLE "account" (
+CREATE TABLE IF NOT EXISTS "account" (
   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   "account_name" varchar(30) NOT NULL,
   "balance" numeric(10, 2) NOT NULL DEFAULT 0.00,
@@ -65,7 +22,7 @@ CREATE TABLE "account" (
   CONSTRAINT account_name_length_check CHECK (char_length(account_name) BETWEEN 3 AND 30)
 );
 
-CREATE TABLE "transaction" (
+CREATE TABLE IF NOT EXISTS "transaction" (
   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   "transaction_name" varchar(30) NOT NULL,
   "amount" numeric(10, 2) NOT NULL,
@@ -78,6 +35,49 @@ CREATE TABLE "transaction" (
   
 );
 COMMIT;
+
+
+
+
+
+-- DEVELOPPEMENT
+-- BEGIN;
+
+-- DROP TABLE IF EXISTS "user", "account", "transaction" CASCADE;
+
+-- CREATE TABLE "user" (
+--   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+--   "username" varchar(30) NOT NULL,
+--   "email" varchar(100) NOT NULL UNIQUE,
+--   "password" varchar(255) NOT NULL,
+--   "created_at" timestamptz NOT NULL DEFAULT now(),
+--   "updated_at" timestamptz,
+--   CONSTRAINT username_length_check CHECK (char_length(username) BETWEEN 3 AND 30)
+-- );
+
+-- CREATE TABLE "account" (
+--   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+--   "account_name" varchar(30) NOT NULL,
+--   "balance" numeric(10, 2) NOT NULL DEFAULT 0.00,
+--   "user_id" smallint NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE,
+--   "created_at" timestamptz NOT NULL DEFAULT now(),
+--   "updated_at" timestamptz,
+--   CONSTRAINT account_name_length_check CHECK (char_length(account_name) BETWEEN 3 AND 30)
+-- );
+
+-- CREATE TABLE "transaction" (
+--   "id" smallint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+--   "transaction_name" varchar(30) NOT NULL,
+--   "amount" numeric(10, 2) NOT NULL,
+--   "type" varchar(10) NOT NULL CHECK (type IN ('revenus', 'depenses')),
+--   "user_id" smallint NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE,
+--   "account_id" smallint NOT NULL REFERENCES "account" ("id") ON DELETE CASCADE,
+--   "created_at" timestamptz NOT NULL DEFAULT now(),
+--   "updated_at" timestamptz,
+--   CONSTRAINT transaction_name_length_check CHECK (char_length(transaction_name) BETWEEN 3 AND 30)
+  
+-- );
+-- COMMIT;
 
 
 
