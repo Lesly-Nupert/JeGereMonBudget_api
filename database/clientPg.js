4790333fe33bf0cc25651dfbc76932a31e9a62f6
Lesly-Nupert <lesly.nupert@gmail.com>
2024-07-14 04:27:37 +0200

    test deploiement de la bdd

--- database/clientPg.js
+++ database/clientPg.js
@@ -1,5 +1,5 @@
 // CONNEXION PostgreSQL via PG
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-module.exports = client;
+// const pg = require('pg');
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.connect();
+// module.exports = client;
